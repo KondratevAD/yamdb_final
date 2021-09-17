af7b21db4e8676dfb0775e1b5b86bfe0dfb50c37
KondratevAD <Kondratev_ad@bk.ru>
2021-09-17 21:24:39 +0300

    fix migrations

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -102,9 +102,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# STATIC_URL = "/static/"
-
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),) #
 
 CORS_URLS_REGEX = r"^/api/.*$"
 
